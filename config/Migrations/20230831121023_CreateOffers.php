a8cd4a014e326e54e272d2851f87918a11ab3b66
martinsebs <martins@eaglebs.com>
2023-08-31 20:12:05 +0800

    Relation offers created

---
+++ config/Migrations/20230831121023_CreateOffers.php
@@ -0,0 +1,55 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateOffers extends AbstractMigration
+{
+
+    public $autoId = false;
+
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('offers');
+        $table->addColumn('id', 'integer', [
+            'autoIncrement' => true,
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('requirements', 'text', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('description', 'text', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('ecpc', 'double', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('click_url', 'text', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addPrimaryKey([
+            'id',
+        ]);
+        $table->create();
+    }
+}
